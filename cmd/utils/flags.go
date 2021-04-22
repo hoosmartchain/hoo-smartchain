1294903fb36cdffe9cf1ba95ffc23a263cd5f187
vvvictorlee <lisheng.ipad@icloud.com>
2021-04-22 23:37:59 +0800

    update chainid

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1534,7 +1534,7 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *eth.Config) {
 	switch {
 	case ctx.GlobalBool(TestnetFlag.Name):
 		if !ctx.GlobalIsSet(NetworkIdFlag.Name) {
-			cfg.NetworkId = 256
+			cfg.NetworkId = 170
 		}
 		cfg.Genesis = core.DefaultTestnetGenesisBlock()
 	case ctx.GlobalBool(DeveloperFlag.Name):
